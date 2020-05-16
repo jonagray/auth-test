bf6349b835e34544554c9153493acbfd6f85327b
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-15 22:03:51 -0700

    token and validator working

--- server/routes/jwtAuth.js
+++ server/routes/jwtAuth.js
@@ -4,63 +4,11 @@ const router = express.Router();
 const pool = require("../../db");
 const bcrypt = require('bcrypt');
 const jwtGenerator = require("../utils/jwtGenerator");
+const validInfo = require("../middleware/validInfo");
 
 // Register
 
-// router.post("/register", async (req, res) => {
-
-//   // 1. Destructure the req.body (name, email, password)
-
-//   const { email, name, password } = req.body;
-
-//   try {
-    
-//     const user = await pool.query("SELECT * FROM nearapogee_users WHERE nearapogee_user_email = $1", [
-//       email
-//     ]);
-
-
-//     if (user.rows.length > 0) {
-//       return res.status(401).send("user already exists");
-//     }
-
-
-//     // 2. Check if the user exists (if user exists, then throw error)
-
-   
-
-    
-
-//     // res.json(user.rows);
-
-//     // 3. Bcrypt the user password
-
-//     const saltRound = 10;
-//     const salt = await bcrypt.genSalt(saltRound);
-
-//     const bcryptPassword = await bcrypt.hash(password, salt);
-
-//     // 4. Enter the new user inside the database
-
-//     let newUser = await pool.query("INSERT INTO nearapogee_users (nearapogee_user_name, nearapogee_user_email, nearapogee_user_password) VALUES ($1, $2, $3) RETURNING *", [
-//       name, email, bcryptPassword]
-//     );
-
-//     res.json(newUser.rows[0]);
-
-//     // 5. Generating the jwt token
-
-//     const token = jwtGenerator(newUser.rows[0].user_id);
-
-//     res.json({token});
-
-//   } catch (error) {
-//     console.error(error.message);
-//     res.status(500).send("Server error");
-//   }
-// })
-
-router.post("/register", async (req, res) => {
+router.post("/register", validInfo, async (req, res) => {
   const { email, name, password } = req.body;
 
   try {
@@ -88,5 +36,36 @@ router.post("/register", async (req, res) => {
     res.status(500).send("Server error");
   }
 });
+
+// Login route
+
+router.post("/login", validInfo, async (req, res) => {
+  const { email, password } = req.body;
+
+  try {
+    const user = await pool.query("SELECT * FROM nearapogee_users WHERE nearapogee_user_email = $1", [
+      email
+    ]);
+
+    if (user.rows.length === 0) {
+      return res.status(401).json("Invalid Credential");
+    }
+
+    const validPassword = await bcrypt.compare(
+      password,
+      user.rows[0].nearapogee_user_password
+    );
+
+    if (!validPassword) {
+      return res.status(401).json("Invalid Credential");
+    }
+    const jwtToken = jwtGenerator(user.rows[0].user_id);
+    return res.json({ jwtToken });
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send("Server error");
+  }
+});
+
 
 module.exports = router;
