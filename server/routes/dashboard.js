28a4dc8148de1c8b4ce833f0e8c8f0a78fd25f05
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-16 19:45:07 -0700

    finished authentication and authorization login logout

--- server/routes/dashboard.js
+++ server/routes/dashboard.js
@@ -7,9 +7,6 @@ const pool = require("../../db");
 router.get("/", authorize, async (req, res) => {
   try {
 
-    
-    // res.json(req.user);
-
     const user = await pool.query("SELECT nearapogee_user_name FROM nearapogee_users WHERE user_id = $1", [req.user.id]
     );
 
@@ -20,26 +17,5 @@ router.get("/", authorize, async (req, res) => {
     res.status(500).send("Server error");
   }
 });
-
-// router.get("/", authorize, async (req, res) => {
-//   try {
-//     const user = await pool.query(
-//       "SELECT nearapogee_user_name FROM nearapogee_users WHERE user_id = $1",
-//       [req.user.id] 
-//     ); 
-    
-//   //if would be req.user if you change your payload to this:
-    
-//   //   function jwtGenerator(user_id) {
-//   //   const payload = {
-//   //     user: user_id
-//   //   };
-    
-//     res.json(user.rows[0]);
-//   } catch (err) {
-//     console.error(err.message);
-//     res.status(500).send("Server error");
-//   }
-// });
 
 module.exports = router;
