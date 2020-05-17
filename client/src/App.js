d36b5fb918f0516bad128697c7b06619b76ebb07
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-16 17:46:09 -0700

    token persistence working

---
+++ client/src/App.js
@@ -0,0 +1,57 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import './App.css';
+
+import {BrowserRouter as Router, Switch, Route, Redirect} from 'react-router-dom';
+
+// Components
+
+import Dashboard from "./components/Dashboard";
+import Login from "./components/Login";
+import Register from "./components/Register";
+
+
+function App() {
+
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const setAuth = (boolean) => {
+    setIsAuthenticated(boolean);
+  }
+
+  async function isAuth() {
+    try {
+
+      const response = await fetch("http://localhost:5000/auth/verify", {
+        method: "POST",
+        headers: {jwt_token : localStorage.token}
+      });
+
+      const parseRes = await response.json();
+
+      parseRes === true ? setIsAuthenticated(true) : setIsAuthenticated(false);
+      
+    } catch (error) {
+      console.error(error.message);
+    }
+  }
+
+  useEffect(() => {
+    isAuth()
+  });
+
+  return (
+    <Fragment>
+      <Router>
+        <div className="container">
+          <Switch>
+            <Route exact path="/login" render={props => !isAuthenticated ? <Login {...props} setAuth={setAuth} /> : <Redirect to="/dashboard" />}/>
+            <Route exact path="/register" render={props => !isAuthenticated ? <Register {...props} setAuth={setAuth} /> : <Redirect to="/login" />}/>
+            <Route exact path="/dashboard" render={props => isAuthenticated ? <Dashboard {...props} setAuth={setAuth} /> : <Redirect to="/login" />}/>
+          </Switch>
+        </div>
+      </Router>
+    </Fragment>
+  );
+}
+
+export default App;
