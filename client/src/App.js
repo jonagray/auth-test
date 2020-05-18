0fbe4f0792c10d428af10b21f8cf6e4f5fdac918
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-17 21:45:18 -0700

    dependencies

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,6 @@
 import React, { Fragment, useState, useEffect } from 'react';
 import './App.css';
+import '../public/favicon.ico';
 
 import {BrowserRouter as Router, Route, Switch, Redirect} from 'react-router-dom';
 
