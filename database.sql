96bed9c5c2d6e558f861393450cf461483574c49
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-18 20:16:35 -0700

    change in database.sql

--- database.sql
+++ database.sql
@@ -1,4 +1,4 @@
-CREATE DATABASE nearapogee_auth;
+-- CREATE DATABASE nearapogee_auth;
 
 -- Create table for login/logout users
 CREATE TABLE nearapogee_users(
@@ -21,4 +21,4 @@ CREATE TABLE users(
 
 INSERT INTO nearapogee_users (nearapogee_user_name, nearapogee_user_email, nearapogee_user_password) VALUES ('jonny', 'jon.a.graybill@gmail.com', 'cat123');
 
-INSERT INTO users (name, email, address) VALUES ('juan', 'jon.a.graybill@gmail.com', '16654 35th Ave S');
+INSERT INTO users (name, email, address) VALUES ('friday', 'friday@gmail.com', '106th St');
