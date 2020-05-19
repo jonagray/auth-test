6932488fa401615755f8830bea83d6f5476ccb88
Jonny Graybill <jon.a.graybill@gmail.com>
2020-05-18 21:46:38 -0700

    router

--- index.js
+++ index.js
@@ -94,6 +94,10 @@ app.delete("/users/:id", async (req, res) => {
 });
 
 app.get("*", (req, res) => {
+  let url= path.join(__dirname, "client/build", "index.html");
+  if (!url.startsWith('/app/')) {
+    url = url.substring(1);
+  }
   res.sendFile(path.join(__dirname, "client/build/index.html"));
 });
 
